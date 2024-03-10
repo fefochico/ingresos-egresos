8e80145cec556887910431c8020dbbe85f9d0555
gabriel <gabriel.yeray@gmail.com>
2024-03-10 11:02:50 +0000

    Se ha añadido la logica al formulario de registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  public registroForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.registroForm= this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+  })
+  }
+
+  crearUsuario(){
+    console.log(this.registroForm);
+    console.log(this.registroForm.value);
+
+  }
 
 }
